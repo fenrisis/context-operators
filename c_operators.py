289941edd9ff45dcd8a6f7d819ec24ea6af3f073
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2024-01-04 11:54:47 +0100

    c_operators.py

---
+++ c_operators.py
@@ -0,0 +1,8 @@
+file_path = r'....\hello_world.txt'
+
+# Записываем построчно в txt
+with open(file_path, 'w') as file:
+    file.write('"Hello"\n\n"world"')
+
+# Очишаем txt
+#open(file_path, 'w').close()
